9143ba6c086179436ddd8ae3096055153779670c
luat <luatncth2007024@fpt.edu.vn>
2021-12-09 20:34:13 +0700

    Practice

---
+++ WebApplication/WebApplication/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string SupplierID { get; set; }
+        public string CategyID { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public string UnitPrice { get; set; }
+        public string UnitslnStock { get; set; }
+        public string UnitsOnOrder { get; set; }
+        public string ReorderLever { get; set; }
+        public string Discontinued { get; set; }
+
+    }
+}
